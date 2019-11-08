b14f3911bd189be915d63620af6d226929cef12f
Charles Pierse <charlespierse@gmail.com>
2019-11-08 00:41:08 +0000

    Only works for the string I tested it on, this was hard

--- Week_3/Charles/challenge3.go
+++ Week_3/Charles/challenge3.go
@@ -5,12 +5,15 @@ import (
 )
 
 func main() {
-	htmlStr := "<h1> This is some <b> text </h1> incorrectly nested </br>"
+	//htmlStr := "<h1> This is some <b> text </h1> incorrectly nested </h1>"
+	htmlStr := "<h1> This is some <b> text </b> correctly nested </h1>"
+	//htmlStr := "<h1> This <div>is</div> some <b> text </b> correctly nested </h1>"
+
+
 	runes := strToRunes(htmlStr)
 	tagRunes, tagIndices := findTags(runes)
 	if testTagParity(tagRunes) {
-		testHTMLValidity(runes,tagIndices)
-
+		testHTMLValidity(runes, tagIndices)
 
 	} else {
 		fmt.Println("Unequal amount of opening and closing tags in string")
@@ -68,16 +71,62 @@ func testTagParity(tagList []rune) bool {
 	// in the array, then we can say the numbers are not equal
 	if float32(sum/len(vals)) == float32(vals[0]) {
 		return true
-	} else {
-		return false
 	}
+
+	return false
 
 }
 
 func testHTMLValidity(htmlRunes []rune, tagIndices []int) bool {
-	fmt.Println(tagIndices)
-	indexStart := tagIndices[0]
-	indexEnd := len(htmlRunes)
-	fmt.Println(indexStart,indexEnd)
-	return false
+	if len(tagIndices) == 0 { //meaning there are only two tags left
+		return true
+	} else {
+		reversedTagIndices := reverseIndex(tagIndices)
+		fmt.Println(tagIndices)
+		// fmt.Println(reversedTagIndices)
+		startTag := getTagContents(htmlRunes, tagIndices[0], tagIndices[1])
+		endTag := getTagContents(htmlRunes, reversedTagIndices[1], reversedTagIndices[0])
+
+		if string(endTag[0]) == "/" { // if the end tag's first char is / then continue
+			if len(endTag[1:]) != len(startTag) {
+				fmt.Printf("Length of End tag %c (exluding '/') is not equal to length of start tag %c, HTML not valid\n", endTag, startTag)
+				return false
+			}
+			for i := range startTag {
+				if startTag[i] != endTag[i+1] {
+					fmt.Printf("%c in start tag is not equal %c in end tag\n", startTag[i], endTag[i+1])
+					return false
+				} else {
+					tagIndices = remove(tagIndices, 1)
+					tagIndices = tagIndices[:len(tagIndices)-2]
+					fmt.Println(tagIndices)
+					testHTMLValidity(htmlRunes, tagIndices)
+				}
+
+			}
+
+		} else {
+			fmt.Printf("End tag %c is not a closing tag to %c, HTML not valid\n", endTag, startTag)
+			return false
+		}
+	}
+
+	return true
+}
+
+func reverseIndex(index []int) []int {
+	reversed := []int{}
+	for i := range index {
+		n := index[len(index)-1-i]
+		reversed = append(reversed, n)
+	}
+	return reversed
+}
+
+func getTagContents(htmlRunes []rune, start, end int) []rune {
+	return htmlRunes[start+1 : end]
+}
+
+func remove(slice []int, i int) []int {
+	return append(slice[:i-1], slice[i+1:]...) // i have no idea what ... does
 }
